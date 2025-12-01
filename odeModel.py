9e068c49ed34e478dca38bbb017a8302ced8f8a0
iLikeTrioxin <dziedzickamil91@gmail.com>
2025-12-02 00:39:41 +0100

    Tweaks to the model and graphs

--- odeModel.py
+++ odeModel.py
@@ -27,8 +27,8 @@ ZONE_VOLUMES[5] = TUBE_AREA * 0.2,
 AIR_DENSITY_AMBIENT = 1.2           # kg/m3 (gas)
 AIR_SPECIFIC_HEAT_CAPACITY = 1000.0         # J/(kg K)
 dH_SO3 = -197e3     # J/mol per mol SO3 formed (-197 kJ/mol)
-A = 1000            # pre-exponential (units depend on rate law)
-E_A = 8e4             # activation energy J/mol
+A = 1e5            # pre-exponential (units depend on rate law)
+E_A = 92000             # activation energy J/mol
 GAS_WALL_HEAT_TRANSFER = 50.0   # W/K (gas-wall coupling per zone)
 WALL_HEAT_CAPACITY = 200.0  # J/K (wall thermal capacitance per zone)
 AMBIENT_LOSS = 1.0       # W/K (wall loss to ambient)
@@ -68,7 +68,7 @@ C_SO3a = SX.sym('C_SO3a', N_ZONES)
 C_SO2a = SX.sym('C_SO2a', N_ZONES)
 C_O2a = SX.sym('C_O2a', N_ZONES)
 
-rate = A * ca.exp(-E_A / (GAS_CONSTANT * Tg)) * C_SO2**2 * C_O2
+rate = (A * ca.exp(-E_A / (GAS_CONSTANT * Tg)) * C_SO2) / (C_SO3+1)
 ADA = AIR_DENSITY_AMBIENT
 ASHC = AIR_SPECIFIC_HEAT_CAPACITY
 GWHT = GAS_WALL_HEAT_TRANSFER
@@ -78,7 +78,7 @@ WHC = WALL_HEAT_CAPACITY
 # k0 * ca.exp(-A_E / (GAS_CONSTANT * temp)) * so2**2 * o2
 # first zone definition
 
-C_SO3a[1:] =-(Fr/ZONE_VOLUMES[1:]) * C_SO3[1:] + rate[1:] + (Fr/ZONE_VOLUMES[1:])*C_SO3[0:-1]
+C_SO3a[1:] = (Fr/ZONE_VOLUMES[1:]) * (C_SO3[0:-1] - C_SO3[1:]) + rate[1:]
 C_SO2a[1:] = (Fr/ZONE_VOLUMES[1:]) * (C_SO2[0:-1] - C_SO2[1:]) - rate[1:]
 C_O2a [1:] = (Fr/ZONE_VOLUMES[1:]) * (C_O2[0:-1] - C_O2[1:]) - 0.5*rate[1:]
 
@@ -88,7 +88,7 @@ Tga[1:] = (Fr / ZONE_VOLUMES[1:]) * (Tg[0:-1] - Tg[1:]) \
         + (GWHT / (ADA * ASHC * ZONE_VOLUMES[1:])) * (Tw[1:] - Tg[1:]) \
         - (0.0 / (ADA * ASHC * ZONE_VOLUMES[1:])) * (Tg[1:] - T_amb)
 
-C_SO3a[0] = (-(Fr/ZONE_VOLUMES[0]) * C_SO3[0] + rate[0])
+C_SO3a[0] = ( (Fr/ZONE_VOLUMES[0]) * (0-C_SO3[0]) + rate[0])
 C_SO2a[0] =(  (Fr/ZONE_VOLUMES[0]) * (C_SO2_in - C_SO2[0]) - rate[0])
 C_O2a[0] = (  (Fr/ZONE_VOLUMES[0]) * (C_O2_in - C_O2[0]) - rate[0])
 
@@ -109,7 +109,7 @@ model.set_rhs('C_O2', C_O2a)
 
 
 # expressions
-rmc = molarConc(25, 1e8)
+rmc = molarConc(K_0+25, 1e8)
 for i in range(N_ZONES):
     model.set_expression(expr_name=f'Cp_so3_{i}', expr=C_SO3[i]/rmc)
     model.set_expression(expr_name=f'Cp_so2_{i}', expr=C_SO2[i]/rmc)
@@ -136,13 +136,13 @@ tvp_num = simulator.get_tvp_template()
 
 # function for time-varying parameters
 def tvp_fun(t_now):
-    tvp_num['T_in'] = K_0 + 50
+    tvp_num['T_in'] = K_0 + 400
     tvp_num['T_amb'] = K_0 + 25
     return tvp_num
 
 # uncertain parameters
-p_num['C_SO2_in'] = molarConc(25, 1e8)*0.2
-p_num['C_O2_in'] = molarConc(25, 1e8)*0.1
+p_num['C_SO2_in'] = molarConc(K_0+25, 1e8)*0.1
+p_num['C_O2_in'] = molarConc(K_0+25, 1e8)*0.1
 def p_fun(t_now):
     return p_num
 
@@ -153,15 +153,15 @@ simulator.setup()
 
 u0 = [
     1, 1, 1, 1, # Q's -Heaters
-    500*1e-6, 500*1e-6, # FO, FB - airflow
+    50*1e-6, 50*1e-6, # FO, FB - airflow
 ]
 
 x0 = []
-x0 += [K_0+50.0 for i in range(N_ZONES)] # (K) wall temperature
-x0 += [K_0+50.0 for i in range(N_ZONES)] # (K) gas temperature
-x0 += [molarConc(25, 1e8)*0.0 for i in range(N_ZONES)] # (mol/m3)
-x0 += [molarConc(25, 1e8)*0.0 for i in range(N_ZONES)] # (mol/m3)
-x0 += [molarConc(25, 1e8)*0.2 for i in range(N_ZONES)]  # (mol/m3)
+x0 += [K_0+400.0 for i in range(N_ZONES)] # (K) wall temperature
+x0 += [K_0+400.0 for i in range(N_ZONES)] # (K) gas temperature
+x0 += [molarConc(K_0+25, 1e8)*0.0 for i in range(N_ZONES)] # (mol/m3)
+x0 += [molarConc(K_0+25, 1e8)*0.1 for i in range(N_ZONES)] # (mol/m3)
+x0 += [molarConc(K_0+25, 1e8)*0.1 for i in range(N_ZONES)]  # (mol/m3)
 
 u0 = np.array(u0).reshape(-1,1)
 x0 = np.array(x0).reshape(-1,1)
@@ -178,17 +178,21 @@ from matplotlib import rcParams
 rcParams['axes.grid'] = True
 rcParams['font.size'] = 18
 
-fig, ax = plt.subplots(N_ZONES+1, sharex=True, figsize=(16,12))
+fig, ax = plt.subplots(nrows=N_ZONES, ncols=2, sharex=True, figsize=(14,10))
 
+plt.tight_layout()
 for i in range(N_ZONES):
-    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_so3_{i}', axis=ax[i], label='SO3')
-    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_so2_{i}', axis=ax[i], label='SO2')
-    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_o2_{i}', axis=ax[i], label='O2')
+    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_so3_{i}', axis=ax[i][0], label='SO3')
+    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_so2_{i}', axis=ax[i][0], label='SO2')
+    mpc_graphics.add_line(var_type='_aux', var_name=f'Cp_o2_{i}', axis=ax[i][0], label='O2')
 
+    mpc_graphics.add_line(var_type='_aux', var_name=f'Tgc_{i}', axis=ax[i][1], label='Tgas')
+    mpc_graphics.add_line(var_type='_aux', var_name=f'Twc_{i}', axis=ax[i][1], label='Twall')
     #label_lines  = mpc_graphics.result_lines['_aux', f'Cp_so3']
     #label_lines += mpc_graphics.result_lines['_aux', f'Cp_so2']
     #label_lines += mpc_graphics.result_lines['_aux', f'Cp_o2']
-    ax[i].legend()
+    ax[i][0].legend()
+    ax[i][1].legend()
 
 
 
